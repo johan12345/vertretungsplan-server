cab947f07ccbf7ca78708d0c0748dca367660b8e
johan12345 <johan.forstner@gmail.com>
2014-10-07 12:52:55 +0200

    Verbesserung der Sicherheit

--- src/com/johan/vertretungsplan/backend/GetVertretungsplanServlet.java
+++ src/com/johan/vertretungsplan/backend/GetVertretungsplanServlet.java
@@ -7,27 +7,56 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
+import com.johan.vertretungsplan.objects.Schule;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+
 @SuppressWarnings("serial")
 public class GetVertretungsplanServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws FileNotFoundException, IOException {	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws FileNotFoundException, IOException {
 		int version = Integer.parseInt(req.getParameter("v"));
-		if(version >= Settings.MIN_SUPPORTED_VERSION) {
+		if (version >= Settings.MIN_SUPPORTED_VERSION) {
 			String schoolId = req.getParameter("school");
 			String regId = req.getParameter("regId");
-			
+
 			MongoClient client = DBManager.getInstance();
 			DB db = client.getDB("vertretungsplan");
-			
-			if (regId != null) { //TODO: remove this 
+
+			if (regId != null) {
 				DBCollection regColl = db.getCollection("registrations");
 				BasicDBObject query = new BasicDBObject("_id", regId);
 				DBObject reg = regColl.findOne(query);
-				if (reg.containsField("password_invalid") && (Boolean) reg.get("password_invalid") == true) {
+				if (reg.containsField("password_invalid") && (Boolean) reg
+								.get("password_invalid") == true) {
+					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					resp.setContentType("text/plain");
+					resp.setCharacterEncoding("UTF-8");
+					resp.getWriter().print("unauthorized");
+					resp.getWriter().close();
+					return;
+				}
+				if (!schoolId.equals((String) reg.get("schoolId"))) {
+					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					resp.setContentType("text/plain");
+					resp.setCharacterEncoding("UTF-8");
+					resp.getWriter().print("unauthorized");
+					resp.getWriter().close();
+					return;
+				}
+			} else {
+				DBCollection coll = db.getCollection("school_info");
+				BasicDBObject query = new BasicDBObject("_id", schoolId);
+				DBObject obj = coll.findOne(query);
+				String jsonString = (String) obj.get("json");
+				Schule school = Schule.fromJSON(schoolId, new JSONObject(
+						jsonString));
+				if (school.getData().has("login")) {
 					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 					resp.setContentType("text/plain");
 					resp.setCharacterEncoding("UTF-8");
@@ -36,12 +65,12 @@ public class GetVertretungsplanServlet extends HttpServlet {
 					return;
 				}
 			}
-			
-			DBCollection coll = db.getCollection("schools");		
+
+			DBCollection coll = db.getCollection("schools");
 			BasicDBObject query2 = new BasicDBObject("_id", schoolId);
 			DBObject school = coll.findOne(query2);
-			
-			if(school != null) {
+
+			if (school != null) {
 				resp.setStatus(HttpServletResponse.SC_OK);
 				resp.setContentType("application/json");
 				resp.setCharacterEncoding("UTF-8");
